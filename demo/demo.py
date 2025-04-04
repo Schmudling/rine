c35cfb08e6793797cb405b7b84ba3489e8a23030
Schmudling <34129880+Schmudling@users.noreply.github.com>
2025-04-04 19:57:36 +0200

    Update demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -1,6 +1,10 @@
+import torch
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 from src.utils import get_transforms, get_our_trained_model
 from PIL import Image
-import torch
 
 device = "cuda:0"
 _, transforms, _ = get_transforms()
@@ -8,7 +12,7 @@ model = get_our_trained_model(ncls=4, device=device)
 model.to(device)
 model.eval()
 
-real_path = "demo/aadlygmazf.jpg"
+real_path = "/content/images/dalle-3/0_0.jpg"
 real_image = Image.open(real_path).convert("RGB")
 real_tensor = transforms(real_image).unsqueeze(0).to(device)
 real_logit = model(real_tensor)[0]
@@ -17,7 +21,7 @@ print(
     f"real image - prob. to be fake: {real_probability.detach().cpu().numpy()[0][0]*100:1.1f}%"
 )
 
-fake_path = "demo/zzrybysskm.png"
+fake_path = "/content/images/dalle-3/0_0.jpg"
 fake_image = Image.open(fake_path).convert("RGB")
 fake_tensor = transforms(fake_image).unsqueeze(0).to(device)
 fake_logit = model(fake_tensor)[0]
@@ -25,3 +29,4 @@ fake_probability = torch.sigmoid(fake_logit)
 print(
     f"fake image - prob. to be fake: {fake_probability.detach().cpu().numpy()[0][0]*100:1.1f}%"
 )
+
